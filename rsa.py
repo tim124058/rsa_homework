37317fe76afc9f5ae1e0cbcf8196f3ebd7836112
tim124058 <tim6989492@gmail.com>
2015-12-25 00:01:37 +0800

    完成加速

--- rsa.py
+++ rsa.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 import random
 
+#用Square-and-Multiply計算(a**b)%m
+def SandM(a,b,m):
+    result = a
+    for i in bin(b)[3:]:
+        result = (result*result) % m
+        if i == '1':
+            result = (result*a) % m
+    return result
+
 #產生n bit的奇數
 def rnd_n_bit(n):
     a="1"
@@ -13,18 +22,18 @@ def rnd_n_bit(n):
 def MillerRabin(n,t):
     if n < 2: return False
     if n==2 or n==3:return True
-    if n%2==0:return False
+    if n%2==0 or n%5==0:return False
     k,m=0,n-1
     while ((m%2)==0):       #找出k 和 m
         k+=1
-        m>>=1
+        m//=2
 
     for i in range(0,t):
         a = random.randint(2,n-2)
-        b = pow(a,m,n)
+        b = SandM(a,m,n)
         if (b != 1) and (b != n-1):
             for j in range(1,k):
-                b = pow(b,2,n)
+                b = (b*b) %n
                 if b == 1: return False
                 elif(b == n-1): break
             else:
@@ -50,6 +59,14 @@ def modInv(e,m):
             return g, x - (b // a) * y, y
     return Ext_Euclid(e,m)[1] % m
 
+
+#CRT加速
+def CRT(x,p,q,d,n,Cp,Cq):
+    Yp = SandM(x % p, d % (p-1), p)
+    Yq = SandM(x % q, d % (q-1), q)
+    return (Cp*Yp+Cq*Yq) % n
+
+
 #輸入bit
 while True:
     bit = input("請輸入rsa的bit數：")
@@ -65,12 +82,18 @@ while True:
 
 #產生p,q
 print("正在產生p,q....")
-p = PrimeGenerator(bit//2,4)
-q = PrimeGenerator(bit//2,4)
-n = p * q
-e = 65537
-phi_n = (p-1) * (q-1)
-d = modInv(e,phi_n) 
+while True:
+    p = PrimeGenerator(bit//2,4)
+    q = PrimeGenerator(bit//2,4)
+    n = p * q
+    phi_n = (p-1) * (q-1)
+    e = 65537
+    if phi_n % e != 0:
+        d = modInv(e,phi_n) 
+        break
+
+Cp = SandM(q,p-2,p) * q
+Cq = SandM(p,q-2,q) * p
 print("p = ",p)
 print("q = ",q)
 print("n = ",n)
@@ -79,11 +102,12 @@ print("e = ",e)
 print("d = ",d)
 
 
-plaintext = int(input("\n\n請輸入明文:"))
+plaintext = int(input("\n\n請輸入明文 : "))
 
-cipher = pow(plaintext,e,n)
+cipher = SandM(plaintext,e,n)
 print("Ciphertext : ",cipher)
 
-plain = pow(cipher,d,n)
+
+plain = CRT(cipher,p,q,d,n,Cp,Cq)
 print("Plaintext : ",plain)
 
