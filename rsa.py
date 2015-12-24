a9de36caca4fe3f1542427b1d73e0c6ababdfd07
tim124058 <tim6989492@gmail.com>
2015-12-24 17:56:36 +0800

    完成rsa基本運算

--- rsa.py
+++ rsa.py
@@ -24,7 +24,7 @@ def MillerRabin(n,t):
         b = pow(a,m,n)
         if (b != 1) and (b != n-1):
             for j in range(1,k):
-                b = (b**2) % n
+                b = pow(b,2,n)
                 if b == 1: return False
                 elif(b == n-1): break
             else:
@@ -39,24 +39,51 @@ def PrimeGenerator(b,t):
             return rnd
 
 
+#找e在mod m下的反函數(d)
+def modInv(e,m):
+    #歐幾里德擴展公式
+    def Ext_Euclid(a, b):
+        if a == 0:
+            return b, 0, 1
+        else:
+            g, y, x = Ext_Euclid(b % a, a)
+            return g, x - (b // a) * y, y
+    return Ext_Euclid(e,m)[1] % m
+
 #輸入bit
 while True:
     bit = input("請輸入rsa的bit數：")
     try:
         bit = int(bit)
         if(bit%256 == 0):
-            print("輸入成功....")
+            print("輸入成功....\n")
             break
         else:
             print("請輸入256的倍數!\n")
     except ValueError:
-        print("只能輸入數字喔！請重新輸入\n")
+        print("只能輸入數字！請重新輸入\n")
 
 #產生p,q
+print("正在產生p,q....")
 p = PrimeGenerator(bit//2,4)
 q = PrimeGenerator(bit//2,4)
 n = p * q
+e = 65537
 phi_n = (p-1) * (q-1)
+d = modInv(e,phi_n) 
 print("p = ",p)
 print("q = ",q)
 print("n = ",n)
+print("phi_n = ",phi_n)
+print("e = ",e)
+print("d = ",d)
+
+
+plaintext = int(input("\n\n請輸入明文:"))
+
+cipher = pow(plaintext,e,n)
+print("Ciphertext : ",cipher)
+
+plain = pow(cipher,d,n)
+print("Plaintext : ",plain)
+
