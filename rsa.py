595e548387308814d89994cde5dabc21c54e9805
tim124058 <tim6989492@gmail.com>
2015-12-25 02:20:16 +0800

    GUI輸入bit和產生p,q

--- rsa.py
+++ rsa.py
@@ -130,18 +130,58 @@ def CRT(x,p,q,d,n,Cp,Cq):
 #                          #
 ############################
 
+
+def GenerateData():
+    bit = Bit_Entry.get()
+    try:
+        bit = int(bit)
+        if(bit%256 == 0):
+            Message.set("輸入成功....")
+        else:
+            Message.set("請輸入256的倍數!")
+            return 
+    except ValueError:
+        Message.set("只能輸入數字！請重新輸入")
+        return 
+
+    #輸入完成，開始產生p,q
+    Message.set("正在產生p,q....")
+    print("正在產生p,q....")
+    while True:
+        p = PrimeGenerator(bit//2,30)
+        q = PrimeGenerator(bit//2,30)
+        n = p * q
+        phi_n = (p-1) * (q-1)
+        e = 65537
+        if phi_n % e != 0:
+            d = modInv(e,phi_n) 
+            break
+
+    Cp = SandM(q,p-2,p) * q
+    Cq = SandM(p,q-2,q) * p
+    Message.set("資料產生完成！")
+
+
+
 root = Tk()
 root.title("RSA")
 
-Bit_Label = Lable(root,width=10,height=5,text="請輸入RSA bit數(n的bit數) : ")
+Bit_Label = Label(root,width=20,height=5,text="請輸入RSA bit數(n的bit數) : ")
 Bit_Label.grid(row=0,column=0)
-bit=IntVar()
-Bit_Entry = Entry(root,width=20,font=("Purisa", 10),textvariable=bit)
+Bit_Entry = Entry(root,width=20,font=("Purisa", 10))
 Bit_Entry.focus_set()
-Bit_Entry.grid(row=0,column=1,columnspan=6,padx=(10,40))
+Bit_Entry.grid(row=0,column=1,padx=(10,10))
 
-Generate_button = Button(root,text ="產生加密資料" )
+Generate_button = Button(root,text ="產生加密資料",command = GenerateData )
+Generate_button.grid(row=0,column=2)
 
+
+
+Message_Label = Label(root,width=15,height=10,font=("Purisa",30),text = "Message : ")
+Message_Label.grid(row=6,column=0)
+Message = StringVar()
+showMessage_Label = Label(root,width=30,height=10,font=("Purisa",30),textvariable=Message)
+showMessage_Label.grid(row=6,column=1,columnspan=3)
 
 
 
